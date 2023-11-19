1d6132fba36c664cdf7223eec4ad74773cb8e3b4
LeonardoFate <alvillamarf@gmail.com>
2023-11-19 17:00:01 -0500

    Agregue el Home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" className="home__social-icon" target="__blank"></a>  
+
+      <a href="" className="home__social-icon" target="__blank"></a>  
+
+      <a href="" className="home__social-icon" target="__blank"></a>  
+    </div>
+  )
+}
+
+export default Social
